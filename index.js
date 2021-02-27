ed1e0a59252585c69cb25eec5e0e089216818cbb
darieloper <darielopper@gmail.com>
2021-02-26 22:14:22 -0500

    send id and message in json format

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const WebSocket = require('ws')
 const port = process.env.PORT || 3000
 const wss = new WebSocket.Server({ port })
 
-var clients = [];
+wss.getUniqueID = function () {
+  function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4();
+};
 
 wss.on('connection', ws => {
-  clients.push(ws)
+  ws.id = wss.getUniqueID();
+  
   ws.on('message', message => {
     console.log(`Received message => ${message}`)
-    clients.forEach(client => client.send('Sent: ' + message))
+    wss.clients.forEach(client => client.send(JSON.stringify({type: 'message', data: message})))
   })
-  ws.send('ho!')
+
+  ws.on('close', function (client) {
+    console.log(client)
+  })
+
+  // Sends back to client the client identifier
+  ws.send(JSON.stringify({type: 'identifier', data: ws.id}))
 })
